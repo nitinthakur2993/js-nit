49dc04ddc864e5253c26c59f3153794ce3a2ddec
niit112 <nitinthakurji56@gmail.com>
2025-06-18 14:13:28 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("Hello mr."); // we are using node js, not browser
+
+console.log(43+5)
+
+console.log("Nitin")
+
+let name = "Nitin"
+let age = 18
+let isLoggedIn = false
+let state = null
+
+
+// number => 2 to power 53
+// bigint
+// string =>""
+// boolean => true/false
+// null => standlone value
+
+// undefined => 
+    //  symbol => for uniqueness
+// objects 
+
+console.log(typeof null); // object
+
+console.log(typeof undefined); // undefined
